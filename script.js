81cc454358716610b2d4f924ca9cd7f8cb97f16c
ChadwickBroski <163284973+ChadwickBroski@users.noreply.github.com>
2026-05-01 10:33:38 +0530

    cleaner settings page

--- script.js
+++ script.js
@@ -41,28 +41,21 @@ function add(){
   document.getElementById("counter").innerHTML = number;
 };
 
-// Get the modal
+// Modal logic
 var modal = document.getElementById("settingsModal");
+var btn   = document.getElementById("settings");
+var span  = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("settings");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.classList.add("open");
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal.classList.remove("open");
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.classList.remove("open");
   }
 }
