2c7df3d046b13483ed4db5d3bde005ccbd57f9bf
ChadwickBroski <163284973+ChadwickBroski@users.noreply.github.com>
2026-05-01 10:42:02 +0530

    changed it back

--- script.js
+++ script.js
@@ -41,21 +41,28 @@ function add(){
   document.getElementById("counter").innerHTML = number;
 };
 
-// Modal logic
+// Get the modal
 var modal = document.getElementById("settingsModal");
-var btn   = document.getElementById("settings");
-var span  = document.getElementsByClassName("close")[0];
 
+// Get the button that opens the modal
+var btn = document.getElementById("settings");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.classList.add("open");
+  modal.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.classList.remove("open");
+  modal.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.classList.remove("open");
+    modal.style.display = "none";
   }
 }
