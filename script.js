d77d3f2051f1c7b22dc43aad7f9b7a24f2460620
ChadwickBroski <163284973+ChadwickBroski@users.noreply.github.com>
2026-05-07 15:55:52 +0530

    new logo

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ const TAGS = {
   6: { label: "Bug Finder", color: "white", bg: "green",     tooltip: "Reported a bug to the owner that has been fixed" },
   // 🔴 MANUAL — set this yourself in Firestore console
   7: { label: "Ideator",    color: "white", bg: "beige",     tooltip: "Contributed by giving an idea to the owner that is currently in the game" },
+  // 🔴 MANUAL — set this yourself in Firestore console
+  8: {label: "Winner",      color: "white", bg: "white",     tooltip: "Won a contest hosted by the owner" }
 };
 
 // Tags that are set manually and must never be overwritten by automatic logic
